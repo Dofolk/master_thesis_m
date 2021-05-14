f785416c096efb28ed1df8a1bd1b09948b67f054
Dofolk <41058334+Dofolk@users.noreply.github.com>
2021-05-14 21:27:53 +0800

    Add files via upload

---
+++ distancelike.m
@@ -0,0 +1,47 @@
+%%
+% load data file and save each info
+Fi = dir([pwd filesep '*.mat']);
+intx = [];inty = [];intz = [];
+for i = 1:length(Fi)
+    load(Fi(i).name);
+    intx = [intx,intfreqX];
+    inty = [inty,intfreqY];
+    intz = [intz,intfreqZ];
+end
+feature(1:200,:) = intx;
+feature(201:400,:) = inty;
+feature(401:600,:) = intz;
+clearvars -except feature
+disp('Part1')
+pause(1)
+%%
+% do kmeans for 4 clustering
+rng(1);
+[idx,cx] = kmeans(feature(1:200,:),4);
+[idy,cy] = kmeans(feature(201:400,:),4);
+[idz,cz] = kmeans(feature(401:600,:),4);
+[id,c] = kmeans(feature,4);
+disp('Part2')
+
+%%
+% build the distance matrix(D)
+l = length(feature);
+D = zeros(l);
+for i =1:l
+    for j = i:l
+        D(i,j) = norm(feature(:,i)-feature(:,j));
+    end
+end
+disp('Part3')
+
+%%
+% build edge matrix
+r = linspace(0,0.1,1000);
+r1 = linspace(0,0.2685,1000);
+E = zeros(length(r),l);
+for i = 1:length(r)
+    T = double((D>0) & (D<r(i)));
+    T = T + T';
+    E(i,:) = sum(T);
+end
+clearvars T
